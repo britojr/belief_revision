b4da22a720be32782bdc541cc867624296df7b7d
Joao Brito <joaojr.89@gmail.com>
2016-06-10 10:08:55 -0300

    add paper and report

---
+++ code/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#from probmod import *
+#import numpy as np
+#import sys
+
+#=============================
+#main
+#=============================
+
+def main():
+	if len(sys.argv) < 2:
+		print "missing input file"
+		exit()
+	
+	#if len(sys.argv) < 3:
+		#test = DatasetManager(sys.argv[2])
+
+main_()
