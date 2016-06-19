ef83481ca70b7f7171f871127e6b347d78a7d827
Joao Brito <joaojr.89@gmail.com>
2016-06-19 18:17:53 -0300

    change all to sympy

--- code/belief_change.py
+++ code/belief_change.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
-#import pycosat
 import itertools
 import copy
-import re
-from sympy.logic.boolalg import to_cnf, And
+from sympy import symbols
+from sympy.logic.boolalg import to_cnf, And, Equivalent, Not
 from sympy.logic.inference import satisfiable
 from sympy.printing.pretty.pretty import PrettyPrinter
 
@@ -13,101 +12,65 @@ def read_logical_file(file_name):
 		cnf_expression = reduce(And, map(to_cnf, [line.strip() for line in fdata if line.strip()]))
 	return cnf_expression
 
-def expression_to_str(expression):
+def expr_to_str(expression):
 	decode_symbols = {
 		u'\xac'		: '~',
 		u'\u2227'	: '&',
 		u'\u2228'	: '|'
 	}
-	s = PrettyPrinter().doprint(expression)
+	print_settings = {
+		"order": None,
+		"full_prec": "auto",
+		"use_unicode": None,
+		"wrap_line": True,
+		"num_columns": 5000,
+		"use_unicode_sqrt_char": True,
+	}
+	s = PrettyPrinter(settings=print_settings).doprint(expression)
 	for symb, dec_symb in decode_symbols.iteritems():
 		s = s.replace(symb, dec_symb)
 	return s
 
-#read a file in DIMACS format and returns a list of sentences
-def read_dimacs(file_name):
-	comment_line = re.compile('c.*')
-	stats_line = re.compile('p\s*cnf\s*(\d*)\s*(\d*)')
-	sentences = []
-	with open(file_name, 'r') as fdata:
-		for line in fdata:
-			line = line.strip()
-			if line and not comment_line.match(line) and not stats_line.match(line):
-				nums = line.strip().split()
-				if nums:
-					nums.pop()
-					sentences.append(map(int, nums))
-	return sentences
+def expr_concat(*expressions):
+	return reduce(And, expressions)
 
-#returns the list of atoms of a given sentence
-def get_atoms(sentences):
-	return set([abs(atom) for clause in sentences for atom in clause])
+#returns the list of atoms of a given expression
+def get_atoms(expression):
+	return expression.atoms()
 
-#change the atoms in the given sentences by the prime value p
-def prime(sentences, atoms, prime_value):
-	primed_sentences = []
-	for clause in sentences:
-		new_clause = []
-		for atom in clause:
-			if abs(atom) in atoms:
-				if atom > 0:	new_clause.append(atom + prime_value)
-				else:				new_clause.append(atom - prime_value)
-			else:
-				new_clause.append(atom)
-		primed_sentences.append(new_clause)
-	return primed_sentences
-
-#replace the old atom value for the new atom value in the list of sentences	
-def replace(sentences, old, new):
-	for clause in sentences:
-		for i in xrange(len(clause)):
-			if abs(clause[i]) == old:
-				if clause[i] > 0:	clause[i] = new
-				else: 				clause[i] = -new
+#replace the old atom value for the new atom value in the expression	
+def replace(expression, old, new):
+	return expression.subs(old, new)
 
 def is_inconsistent(expression):
 	return not satisfiable(expression)
 
-#equivalence: a <=> b == [[a, -b], [-a, b]]
-def equivalence(a, b):
-	return [[a, -b], [-a, b]]
+def primed(atom):
+	return symbols(str(atom)+"_pr")
 
-#returns a revised base
-def simple_revision(knowlege_base, revision_clauses):
-	if is_inconsistent(knowlege_base) or is_inconsistent(revision_clauses):		return None
-	atom_in = []
-	atom_out = []
-	conflict_atoms = get_atoms(knowlege_base).intersection(get_atoms(revision_clauses))
-	prime_value = max(max(get_atoms(knowlege_base)), max(get_atoms(revision_clauses)))
-	revised_base = prime(knowlege_base, conflict_atoms, prime_value)
-	union_base = revised_base + revision_clauses
-	for a in conflict_atoms:
-		union_base.extend(equivalence(a, a+prime_value))
-		if is_inconsistent(union_base):
-			atom_out.append(a)
-			del union_base[-2:]
-		else:
-			atom_in.append(a)
-	for a in atom_in:
-		replace(revised_base, a+prime_value, a)
-	for a in atom_out:
-		replace(revised_base, a+prime_value, -a)
-	revised_base.extend(revision_clauses)
-	return revised_base
+#change the atoms in the given expression by the prime value p
+def prime(expression, atoms):
+	primed_expression = copy.deepcopy(expression)
+	for atom in atoms:
+		primed_expression = replace(primed_expression, atom, primed(atom))
+	return primed_expression
+
+#equivalence: a <=> b == a >> b & a << b
+def equivalence(a, b):
+	return to_cnf(Equivalent(a, b))
 
 #returns the equivalence clauses for each atom and its respective prime value
-def get_equivalence_clauses(atoms, prime_value):
-	equivalences = []
-	for a in atoms:
-		equivalences.extend(equivalence(a, a+prime_value))
-	return equivalences
+def get_equivalence_clauses(atoms):
+	return reduce(And, [equivalence(a, primed(a)) for a in atoms])
+	#return expr_concat([equivalence(a, primed(a)) for a in atoms])
 
 #returns a list of the maximal equivalence sets
-def get_max_equivalence_sets(union_base, conflict_atoms, prime_value):
+def get_max_equivalence_sets(union_base, conflict_atoms):
 	minimal_inconsistent_sets = []
 	maximal_consistent_sets = []
 	
 	#iterate through the possible combinations of atoms, descending on size
+	#for set_size in reversed(xrange(len(conflict_atoms), 0, -1)):
 	for set_size in xrange(len(conflict_atoms), 0, -1):
 		comb_list = itertools.combinations(conflict_atoms, set_size)
 		for comb in comb_list:
@@ -129,7 +92,7 @@ def get_max_equivalence_sets(union_base, conflict_atoms, prime_value):
 			if consist:	continue
 			
 			#test if the formula (union_base and this set) is satisfiable
-			if is_inconsistent(union_base + get_equivalence_clauses(atoms_set, prime_value)):
+			if is_inconsistent(expr_concat(union_base, get_equivalence_clauses(atoms_set))):
 				#add a minimal inconsistent set and remove its supersets
 				for inc_set in minimal_inconsistent_sets:
 					if atoms_set.issubset(inc_set):
@@ -148,18 +111,17 @@ def get_max_equivalence_sets(union_base, conflict_atoms, prime_value):
 def revision(knowlege_base, revision_clauses):
 	if is_inconsistent(knowlege_base) or is_inconsistent(revision_clauses):		return None
 	conflict_atoms = get_atoms(knowlege_base).intersection(get_atoms(revision_clauses))
-	prime_value = max(max(get_atoms(knowlege_base)), max(get_atoms(revision_clauses)))
-	primed_base = prime(knowlege_base, conflict_atoms, prime_value)
+	primed_base = prime(knowlege_base, conflict_atoms)
 	
-	union_base = primed_base + revision_clauses
-	maximal_consistent_sets = get_max_equivalence_sets(union_base, conflict_atoms, prime_value)
+	union_base = expr_concat(primed_base, revision_clauses)
+	maximal_consistent_sets = get_max_equivalence_sets(union_base, conflict_atoms)
 	
 	revised_base_list = []
 	for equivalence_set in maximal_consistent_sets:
 		revised_base_list.append(copy.deepcopy(union_base))
 		for atom in conflict_atoms:
 			if atom in equivalence_set:
-				replace(revised_base_list[-1], atom+prime_value, atom)
+				revised_base_list[-1] = replace(revised_base_list[-1], primed(atom), atom)
 			else:
-				replace(revised_base_list[-1], atom+prime_value, -atom)
+				revised_base_list[-1] = replace(revised_base_list[-1], primed(atom), Not(atom))
 	return revised_base_list
--- code/main.py
+++ code/main.py
@@ -5,21 +5,12 @@ from belief_change import *
 #=============================
 #main
 #=============================
-
-def write_cnf(file_name, sentences):
-	fout = open(file_name, "w")
-	for clause in sentences:
-		s="("
-		signal=""
-		for i, atom in enumerate(clause):
-			if i: signal="+"
-			if atom > 0:
-				s+= "{}{}".format(signal, atom)
-			else:
-				s+= "{}~{}".format(signal, abs(atom))
-		s+=")"
-		fout.write(s + "\n")
-	fout.close()
+	
+def print_base(expression):
+	lines = expr_to_str(expression).split('&')
+	for line in lines:
+		print line.strip()
+	print
 
 def main():
 	if len(sys.argv) < 3:
@@ -30,17 +21,20 @@ def main():
 	knowledge_base = read_logical_file(sys.argv[1])
 	revision_sentence = read_logical_file(sys.argv[2])
 	
-	print "initial base:\n{}".format(expression_to_str(knowledge_base))
+	#print "initial base:"
+	#print_base(knowledge_base)
 	if is_inconsistent(knowledge_base):
 		raise ValueError("original base is inconsistent")
-	print "new information:\n{}".format(revision_sentence)
+	#print "new information:"
+	#print_base(revision_sentence)
 	if is_inconsistent(revision_sentence):
 		raise ValueError("new information is inconsistent")
 	
-	#revised_list = revision(sentences, new_info)
-	#for i, revised in enumerate(revised_list):
-		#print "revised base #{}:\n{}".format(i+1, revised)
-		#if is_inconsistent(revised):
-			#raise ValueError("revised base is inconsistent")
+	revised_list = revision(knowledge_base, revision_sentence)
+	for i, revised in enumerate(revised_list):
+		#print "revised base #{}:".format(i+1)
+		#print_base(revised)
+		if is_inconsistent(revised):
+			raise ValueError("revised base is inconsistent")
 	
 main()
